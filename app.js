471ad125e6b60cc70ae236392f7947bacab73c7f
santhosh <santhosh.003z@gmail.com>
2025-05-29 22:29:17 +0530

    feat: changed express server to http server

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const http = require("http");
+const io = require("./socket");
 const path = require("path");
 const assetPath = path.join(__dirname, "public");
 const cors = require("cors");
@@ -6,6 +8,9 @@ const router = require("./routes/route");
 require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
+io(server);
+
 app.use(express.static(assetPath));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -13,4 +18,4 @@ app.use(cors());
 
 app.use(router);
 
-module.exports = app;
+module.exports = server;
